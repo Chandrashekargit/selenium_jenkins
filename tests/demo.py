34b8fdae5d9c8507e2f2627054f95372f3cecaf8
chandrashekargit <chandrashkr.dot@gmail.com>
2022-06-04 13:24:57 +0530

    2nd commit

---
+++ tests/demo.py
@@ -0,0 +1,12 @@
+import time
+from selenium import webdriver
+
+
+def driver():
+    driver = webdriver.Chrome(executable_path="/home/we45-lt24/ASE_UI_Automation/venv/bin/chromedriver_linux64/chromedriver")
+    driver.maximize_window()
+    driver.get("https:www.google.com")
+    time.sleep(5)
+
+driver()
+
